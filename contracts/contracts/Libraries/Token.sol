a52f4311f335d9d6e9d7dcb9aac959b8517672c2
Mick de Graaf <mick@mickdegraaf.nl>
2022-10-29 17:27:42 +0200

    oopsie

--- contracts/contracts/Libraries/Token.sol
+++ contracts/contracts/Libraries/Token.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Token {
-    function transferToMsgSender(address token, address to, uint256 amount) external {
-        IERC20(token).transfer(to, amount);
+    function transferToMsgSender(address token, uint256 amount) external {
+        IERC20(token).transfer(msg.sender, amount);
     }
 
-    function transferETHToMsgSender(address to, uint256 amount) external {
-        payable(to).transfer(amount);
+    function transferETHToMsgSender(uint256 amount) external {
+        msg.sender.transfer(amount);
     }
 }
